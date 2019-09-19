b0e4a0eb8ca6cd7fb10f6707134afaf7e048b8ad
BitoGx <RizqiGx@gmail.com>
2019-09-20 06:43:05 +0700

    Function Upload, Display and Connection Done
    fungsi upload beres tinggal perbaikan validasi, display sudah keluar tinggal di integrasi

--- test.php
+++ test.php
@@ -1,10 +1,20 @@
 <?php
+  include "php/connection.php";
 ?>
 <html>
   <head>
     <title>Test Page</title>
   </head>
   <body>
-    <h1>TEST PAGE</h1>
+    <center>
+      <h1>TEST PAGE</h1>
+      <form action="php/upload.php" method="post" enctype="multipart/form-data"> Select file to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload File" name="submit">
+      </form>
+    </center>
+    <?php
+      include "php/display.php";
+    ?>
   </body>
 </html>
