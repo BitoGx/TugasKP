1bd72f283155474bbf9190c9761fd7c6c6e1aa2c
BitoGx <RizqiGx@gmail.com>
2019-10-14 20:28:51 +0700

    Display update
    Display watiting for resize

--- php/display.php
+++ php/display.php
@@ -1,11 +1,55 @@
 <?php
-
-//Get a list of file paths using the glob function.
-$fileList = glob('../uploads/*');
-
-//Loop through the array that glob returned.
-foreach($fileList as $filename){
-   //Simply print them out onto the screen.
-   echo $filename, '<br>'; 
-}
+  
+  //Memanggil Connection.php
+  require_once "connection.php";
+  
+  //Memilih database
+  mysqli_select_db($conn,"tubesKP");
+  
+  //Mempersiapkan Command Query  untuk mengambil data IdUser,Nama,Level berdasarkan Username dan Password
+  $sql="select R.Judul,R.Author,R.Tahun_Dibuat,R.Tanggal_Unggah,A.Bagian,A.Nama,R.Id_Buku from repo as R, admin as A where R.Id_Admin = A.Id_Admin";
+  
+  //Menjalankan perintah query dan menyimpannya dalam variabel hasil
+  $hasil=mysqli_query ($conn,$sql);
+  
+  //Mengambil 1 baris hasil dari perintah query
+  $row=mysqli_fetch_row($hasil);
+  
+  if($row)
+  {
+    echo "<table border='1'>
+            <tr>
+              <td> Judul </td>
+              <td> Author </td>
+              <td> Tahun Dibuat </td>
+              <td> Tanggal Unggah </td>
+              <td> Bagian </td>
+              <td> Penanggung Jawab </td>
+              <td> Baca Online </td>
+              <td> Download </td>
+            </tr>";
+    do
+    {
+      list($Judul,$Author,$Tahun,$Tanggal,$Bagian,$Nama,$IdBuku)=$row;
+      $Judul = ucwords($Judul);
+      echo "<form action='php/hapus_barang.php' method='post' onsubmit='return FormValidation()'>";
+      echo "<tr>
+              <td> $Judul </td>
+              <td> $Author </td>
+              <td> $Tahun </td>
+              <td> $Tanggal </td>
+              <td> $Bagian </td>
+              <td> $Nama </td>
+              <td>  </td>
+              <td></td>
+            </tr>";
+      echo "</form>";
+    }
+    while($row=mysqli_fetch_row($hasil));
+    echo "</table>";
+  }
+  else
+  {
+    echo "<center><h1>Tidak ada Dokumen</h1></center>";
+  }
 ?>
